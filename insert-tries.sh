303888a09860d2f6a43aece6a88c824b476d52f1
Kamil Jezek <kamil.jezek@gmail.com>
2021-02-05 15:27:07 +1100

    heap size 8GB only

--- insert-tries.sh
+++ insert-tries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HEAP_SIZE=32896
+HEAP_SIZE=8192
 DB_PATH="db-bucket-tree"
 BATCH_SIZE=1000
 INPUT_TRIE="csv_dump/trie_11000000_11_0xbb91d90516ce901742bfd5d73e4bf99b510776ef941e1f86ea8a1fd7d0d2e2c5.csv"
