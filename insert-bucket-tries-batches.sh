78410411b9e4a8bfd38f07b9371ab6c1cae9f123
Kamil Jezek <kamil.jezek@gmail.com>
2021-02-11 15:34:15 +1100

    fixed missing bracket

--- insert-bucket-tries-batches.sh
+++ insert-bucket-tries-batches.sh
@@ -10,7 +10,7 @@ i=1
 #for i in 1 10 100 1000 10000 100000 1000000 10000000; do
   for max_height in 3 5 7 9 11; do
     rm -rf $DB_PATH_BUCKET_TRIE
-    START_TIME=$(date +%s
+    START_TIME=$(date +%s)
     node --max-old-space-size=$HEAP_SIZE bucket-trie-insert.js $DB_PATH_BUCKET_TRIE $PARALLELISM $i $max_height $INPUT_TRIE
     END_TIME=$(date +%s)
     echo "Total time $((END_TIME - START_TIME))s"
